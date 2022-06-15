976cb97db5a980bb0b34d127777960e08dfe7ee6
Emmanuel anchia <emmanuelanchialobo@hotmail.com>
2022-06-14 19:12:17 -0600

    Laboratorio 6

--- Lab5/Controllers/PlanetasController.cs
+++ Lab5/Controllers/PlanetasController.cs
@@ -1,4 +1,5 @@
 ﻿using Lab5.Handlers;
+using Lab5.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,12 +10,95 @@ namespace Lab5.Controllers
 {
     public class PlanetasController : Controller
     {
-        // GET: Planetas
+       
         public ActionResult listadoDePlanetas()
         {
             PlanetasHandler accesoDatos = new PlanetasHandler();
             ViewBag.planetas = accesoDatos.obtenerTodoslosPlanetas();
             return View();
+        }
+
+      
+        public ActionResult crearPlaneta()
+        {
+            return View();
+        }
+
+       
+        [HttpPost]
+        public ActionResult crearPlaneta(PlanetaModel planeta)
+        {
+            ViewBag.ExitoAlCrear = false;
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    PlanetasHandler accesoDatos = new PlanetasHandler();
+                    ViewBag.ExitoAlCrear = accesoDatos.crearPlaneta(planeta);
+
+                    if (ViewBag.ExitoAlCrear)
+                    {
+                        ViewBag.Message = "El planeta" + " " + planeta.nombre + " fue creado con exito :)";
+                        ModelState.Clear();
+                    }
+                }
+                return View();
+            }
+            catch
+            {
+                ViewBag.Message = "Algo salio mal y no fue posible crear el planeta :c";
+                return View();
+            }
+        }
+
+        
+        [HttpGet]
+        public ActionResult editarPlaneta(int? identificador)
+        {
+            ActionResult vista;
+            try
+            {
+                PlanetasHandler accesoDatos = new PlanetasHandler();
+                PlanetaModel planetaModificar = accesoDatos.obtenerTodoslosPlanetas().Find(smodel => smodel.id == identificador);
+                if (planetaModificar == null)
+                {
+                    vista = RedirectToAction("listadoDePlanetas");
+                }
+                else
+                {
+                    vista = View(planetaModificar);
+                }
+            }
+            catch
+            {
+                vista = RedirectToAction("listadoDePlanetas");
+            }
+            return vista;
+        }
+
+        [HttpPost]
+        public ActionResult editarPlaneta(PlanetaModel planeta)
+        {
+            try
+            {
+                PlanetasHandler accesoDatos = new PlanetasHandler();
+                accesoDatos.modificarPlaneta(planeta);
+                return RedirectToAction("Index", "Home");
+            }
+            catch
+            {
+                ViewBag.Message = "Algo salio mal y no fue posible modificar el planeta :c";
+                return View();
+            }
+        }
+
+        [HttpGet]
+        public FileResult accederArchivo(int identificador)
+        {
+            PlanetasHandler accesoDatos = new PlanetasHandler();
+            var tupla = accesoDatos.descargarContenido(identificador);
+            return File(tupla.Item1, tupla.Item2);
+           
         }
     }
 }
--- Lab5/Models/PlanetaModel.cs
+++ Lab5/Models/PlanetaModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,9 +9,23 @@ namespace Lab5.Models
     public class PlanetaModel
     {
         public int id { get; set; }
+
+        [Required(ErrorMessage = "Ingrese el nombre del planeta")]
+        [Display(Name = "Ingrese el nombre del planeta")]
         public string nombre { get; set; }
+        [Required(ErrorMessage = "Necesario que indique el tipo de planeta")]
+        [Display(Name = "Ingrese el yipo del planeta")]
         public string tipo { get; set; }
+        [Required(ErrorMessage = "Necesario que indique el numero de anillos")]
+        [Display(Name = "Ingrese el numero de anillos del planeta")]
+        [RegularExpression("^[0-9]*$", ErrorMessage = "Debe ingresar numeros")]
         public int numeroAnillos { get; set; }
         // de momento no se va trabajar con el archivo...
+        [Required(ErrorMessage = "Debe agregar un archivo")]
+        [Display(Name = "Ingrese el archivo con los detalles del planeta")]
+        public HttpPostedFileBase archivo { get; set; }
+
+        public string tipoArchivo { get; set; }
+
     }
 }
